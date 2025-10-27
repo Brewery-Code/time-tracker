73e1807b2d1bf30b464aee3db02e55d9b0ceb232
Oleksandr Yakymchuk <oleksandr_yakymchuk@protonmail.com>
2025-10-27 08:13:41 +0200

    feat: Add docker configuration. Add configuration for db connect.

---
+++ backend/src/database.py
@@ -0,0 +1,20 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+from decouple import config
+
+
+DB_URL = f"postgresql://{config('POSTGRES_USER')}:{config('POSTGRES_PASSWORD')}@{config('POSTGRES_HOST')}:5432/{config('POSTGRES_DB')}"
+
+engine = create_async_engine(DB_URL)
+new_session = async_sessionmaker(engine, expire_on_commit=False)
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    """Return DB session."""
+    async with new_session() as session:
+        yield session
+
+
+class Base(DeclarativeBase):
+    pass
--- backend/src/main.py
+++ backend/src/main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("src.main:app", reload=True)
+    uvicorn.run("src.main:app",host="0.0.0.0", port=8000, reload=True)
