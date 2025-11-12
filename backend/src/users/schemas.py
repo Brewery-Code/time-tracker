f53a901d780c67b9a618d602dcebde863aad610d
Oleksandr Yakymchuk <oleksandr_yakymchuk@protonmail.com>
2025-11-12 12:15:01 +0200

    fix(users): Remove full_name field. Add first_name and last_name fields. Change business logic of endpoints.

--- backend/src/users/schemas.py
+++ backend/src/users/schemas.py
@@ -1,15 +1,25 @@
 from datetime import datetime
 
-from pydantic import BaseModel, EmailStr, Field
+from fastapi import HTTPException
+from pydantic import BaseModel, EmailStr, Field, field_validator, model_validator
+from pydantic_core.core_schema import ValidationInfo
 
 
 class UserCreateSchema(BaseModel):
     """
     Schema used when creating a new user.
     """
-    full_name: str = Field(max_length=100)
+    first_name: str
+    last_name: str
     email: EmailStr
-    password: str = Field(min_length=8)
+    password1: str = Field(min_length=8)
+    password2: str = Field(min_length=8)
+
+    @model_validator(mode="after")
+    def check_passwords_match(self):
+        if self.password1 != self.password2:
+            raise ValueError("Passwords do not match")
+        return self
 
 
 class UserLoginSchema(BaseModel):
@@ -25,7 +35,8 @@ class UserCurrentSchema(BaseModel):
     Schema used for current user return.
     """
     id: int
-    full_name: str
+    first_name: str
+    last_name: str
     email: EmailStr
     created_at: datetime
 
